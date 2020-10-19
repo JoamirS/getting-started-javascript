8a1c4a909f5c24ed4259b4df4da836825fef0ee6
JoamirS <jmrsilveira@outlook.com>
2020-10-18 22:12:27 -0300

    Using ifs and Switch case

---
+++ Class_10.js
@@ -0,0 +1,49 @@
+var hourNow = new Date()
+var Hour = hourNow.getHours()
+
+console.log(`Agora são exatamente ${Hour} horas.`)
+
+if (Hour < 12) {
+    console.log('Bom dia!')
+} else if (Hour < 18) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noite!')
+}
+
+var getDate = new Date()
+var dayOfTheWeek = getDate.getDay()
+
+/*
+0 - Domingo
+1 - Segunda
+2 - Terça
+3 - Quarta
+4 - Quinta
+5 - Sexta
+6 - Sábado
+*/
+
+switch (dayOfTheWeek) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+}
