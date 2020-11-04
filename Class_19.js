24936077d774f4e31a5ef74e47aab8305abe5473
JoamirS <jmrsilveira@outlook.com>
2020-11-04 09:43:54 -0300

    compound variables

---
+++ Class_19.js
@@ -0,0 +1,9 @@
+var valores = [8, 1, 7, 4, 2, 9]
+
+for (var position = 0; position < valores.length; position++) {
+    console.log(`A posição ${position} tem o valor ${valores[position]}`)
+}
+
+for (var position in valores){
+    console.log(`A posição ${position} tem o valor ${valores[position]}`)
+}
